1959bb80e1cc88f9febaca587c45c249f3615270
em <6em.rva@gmail.com>
2018-11-04 18:32:14 -0500

    some improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Portfolio from './components/Portfolio';
 
+
 class App extends Component {
   render() {
     return (
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-grid-system';
 import { Parallax, Background } from 'react-parallax';
-import { Link } from 'react-router-dom';
+import GitHubLogo from './images/github-logo.png';
+import FitText from 'react-fittext';
 
 const Portfolio = () => (
     <div>
-        <div>
-            <h1>EMILY MURPHY</h1>
-        </div>
+        <Container>
+            <Row>
+                <Col sm={12} md={12} lg={12}>
+                    <FitText compressor={1}>
+                    <h1 style={{ fontFamily: "'Cinzel', serif", textAlign: 'center', padding: 'none'}}>EMILY MURPHY</h1>
+                    </FitText>
+                </Col>
+            </Row>
+        </Container>        
         <Parallax
             bgImage={require('./images/1.jpg')}
             bgImageAlt='an arial view of a sailboat on green water'
@@ -14,10 +22,13 @@ const Portfolio = () => (
         >
         <div style={{height: '800px' }} /> 
         </Parallax>
-        <div style={{height: '500px', padding: '20px'}}>
-            <h1 style={{margin: '10px'}}>EMILY MURPHY</h1>
-            
-        </div>
+        <Container style={{ padding: '20px'}}>
+            <Row>
+                <FitText compressor={2}>
+                <Col sm={12} md={12} lg={12}><h2 style={{ fontFamily: "'Cinzel', serif", margin: '10px'}}>ABOUT</h2></Col>
+                </FitText>
+            </Row>
+        </Container>
         <Parallax
             bgImage={require('./images/2.jpg')}
             bgImageAlt='an arial view of two ships at port'
@@ -25,10 +36,13 @@ const Portfolio = () => (
         >
         <div style={{height: '800px'}} />
         </Parallax>
-        <div style={{height: '500px', padding: '20px'}}>
-            <h1 style={{margin: '10px'}}>EMILY MURPHY</h1>
-            
-        </div>
+        <Container style={{ padding: '20px'}}>
+            <Row>
+                <FitText compressor={2}>
+                <Col sm={12} md={12} lg={12}><h2 style={{ fontFamily: "'Cinzel', serif", margin: '10px'}}>SKILLS</h2></Col>
+                </FitText>
+            </Row>
+        </Container>
         <Parallax
             bgImage={require('./images/3.jpg')}
             bgImageAlt='an arial view of docks leading up to houses'
@@ -36,10 +50,17 @@ const Portfolio = () => (
         >
         <div style={{height: '800px'}} />
         </Parallax>
-        <div style={{height: '500px', padding: '20px'}}>
-            <h1 style={{margin: '10px'}}>EMILY MURPHY</h1>
-            
-        </div>
+
+        {/* PORTFOLIO */}
+        <Container style={{ padding: '20px'}}>
+            <Row>
+                <FitText compressor={2}>
+                <Col sm={12} md={12} lg={12}><h2 style={{ fontFamily: "'Cinzel', serif", margin: '10px'}}>PORTFOLIO</h2></Col>
+                </FitText>
+            </Row>
+        </Container>
+
+
         <Parallax
             bgImage={require('./images/4.jpg')}
             bgImageAlt='an arial view of a neighborhood with red and blue houses '
@@ -47,20 +68,28 @@ const Portfolio = () => (
         >
         <div style={{height: '800px'}} />
         </Parallax>
-        <div style={{height: '500px', padding: '20px'}}>
-            <h1 style={{margin: '10px'}}>EMILY MURPHY</h1>
-            
-        </div>
+        <Container style={{ padding: '20px'}}>
+            <Row>
+                <FitText compressor={2}>
+                <Col sm={12} md={12} lg={12}><h2 style={{ fontFamily: "'Cinzel', serif", margin: '10px'}}>CONTACT</h2></Col>
+                </FitText>
+            </Row>
+            <Row>
+                <Col sm={6} md={6} lg={6}><a style={{ alignSelf: 'flex-end' }} href='https://github.com/elm9'> <img style={{ width: '80px' }} src={GitHubLogo} /> </a></Col>
+            </Row>
+        </Container>
         <Parallax
             bgImage={require('./images/5.jpg')}
-            bgImageAlt='an arial view of roads, houses, and trees'
+            bgImageAlt='an arial view of red rooftops'
             strength={600}
         >
         <div style={{height: '800px'}} />
         </Parallax>
-        <div>
-            <p style={{fontSize: 'small'}}>Images by <a href={'https://www.pexels.com/@pok-rie-33563'}>Pok Rie</a></p>
-        </div>
+        <Container>
+            <Row>
+                <Col sm={12} md={12} lg={12}><p style={{fontSize: 'small', textAlign: 'center'}}>Images by <a href={'https://www.pexels.com/@pok-rie-33563'}>Pok Rie</a></p></Col>
+            </Row>
+        </Container>
     </div>
 );
 
